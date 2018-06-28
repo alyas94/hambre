02ad171cf2cec246cacf49e0c61bb80247b20b87
DanFein <danfeinstat@gmail.com>
2018-06-28 15:16:07 -0700

    added info window, added if statement to getcurrentlocation in onload

--- client/src/components/Map/MapComponent.js
+++ client/src/components/Map/MapComponent.js
@@ -3,11 +3,16 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
   state = {
-    centerLat: 38.5727,
-    centerLng: -121.4679,
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+    centerLat: "",
+    centerLng: "",
     trucks: [
       {
         name: "Temple Coffee",
+        type: "Coffee",
+        description: "Great coffee, questionable pastries.",
         position: {
           lat: 38.5639,
           lng: -121.4724,
@@ -15,6 +20,8 @@ export class MapContainer extends Component {
       },
       {
         name: "Miyagi Bar & Sushi",
+        type: "Japanese",
+        description: "mediocre japanese food!",
         position: {
           lat: 38.5734,
           lng: -121.4022,
@@ -24,15 +31,39 @@ export class MapContainer extends Component {
   };
 
   componentDidMount() {
-    this.getCurrentLocation();
-    let athing = this.props;
-    console.log(athing);
+    if (this.state.centerLat === "" && this.state.centerLng === "") {
+      this.getCurrentLocation();
+    }
+    // let athing = this.props;
+    // console.log(athing);
   }
 
   componentDidUpdate() {
     // this.centerMoved(mapProps, map);
     console.log("component updated!!");
   }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
+  onInfoWindowClose = () =>
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false,
+    });
 
   fuckYouGoogleApi = (mapProps, map) => {
     const { google } = mapProps;
@@ -72,11 +103,15 @@ export class MapContainer extends Component {
         }}
         zoom={14}
         onReady={this.fuckYouGoogleApi}
+        onClick={this.onMapClicked}
       >
         {this.state.trucks.map(truck => {
           return (
             <Marker
+              onClick={this.onMarkerClick}
               name={truck.name}
+              type={truck.type}
+              description={truck.description}
               position={truck.position}
               icon={{
                 url: "../../../truck-catering.png",
@@ -86,6 +121,17 @@ export class MapContainer extends Component {
             />
           );
         })}
+        <InfoWindow
+          onClose={this.onInfoWindowClose}
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+            <h2>{this.state.selectedPlace.type}</h2>
+            <p>{this.state.selectedPlace.description}</p>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
