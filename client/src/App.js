58dcfdf529bff87b1acffae6aac21431ab86522c
Benjamin Dally <benjamin.dally@icloud.com>
2018-07-03 09:37:30 -0700

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 // import MapComponent from "./components/Map/MapComponent";
 import MapPage from "./pages/MapPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import signup from "./components/SignupLogin/signup";
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={MapPage} />
+      <Route exact path="/signup" component={signup} />
     </div>
   </Router>
 );
