f2377e4ade09b84ec3d64527ce4ea114c6356095
Gurnam Sunny <Sunny@Sunnys.local>
2018-07-03 19:49:04 -0700

    footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import LoveIcon from "./LoveIcon.png"
+
+const footerStyle = {
+  backgroundColor: "#666",
+  fontSize: "20px",
+  color: "white",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "relative",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+
+function Footer({ children }) {
+  return (
+    <div>
+          <div style={footerStyle}>{children}
+              < Typography color = "inherit"
+                  varient = "subheading" >©Hambre. Made with
+              <img src={LoveIcon} height="20"/> in Sacramento.
+              </Typography>
+            
+            
+             
+          </div>
+          
+    </div>
+  );
+}
+
+export default Footer;
