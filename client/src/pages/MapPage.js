63b8880957bf57075de5b9ccfb7c510aaddd7a45
DanFein <danfeinstat@gmail.com>
2018-07-10 11:33:43 -0700

    added media queries for responsive mainpage

--- client/src/pages/MapPage.js
+++ client/src/pages/MapPage.js
@@ -4,28 +4,40 @@ import MapSidebar from "../components/Map/MapSidebar";
 import Navbar from "../components/Navbar/Navbar";
 import { LandingLogo } from "../components/Logo/LandingLogo";
 import { MissionStatement } from "../components/MissionStatement/MissionStatement";
-import Footer from "../components/Footer/Footer";
+import Grid from "@material-ui/core/Grid";
 // import FilterDrawer from "../components/FilterDrawer/FilterDrawer";
 
 class MapPage extends Component {
   render() {
     return (
       <div className="row map-page-wrapper">
-        <Navbar />
-        <div className="parallax-wrapper">
-          <section className="section parallax bg1">
-            <LandingLogo />
-          </section>
-          <section className="section">
-            <MapSidebar />
-            <MapContainer />
-          </section>
-          <section className="section parallax bg2" />
-          <section className="section">
-            <MissionStatement />
-          </section>
-          {/* <section className="section parallax bg4" /> */}
-        </div>
+        <Grid container spacing={8}>
+          <Grid item xs={12}>
+            <Navbar />
+          </Grid>
+          <div className="parallax-wrapper">
+            <section className="section parallax bg1">
+              <Grid item xs={12}>
+                <LandingLogo />
+              </Grid>
+            </section>
+            <section className="section">
+              <Grid item xs={4} sm={12}>
+                <MapSidebar />
+              </Grid>
+              <Grid item xs={8} sm={12}>
+                <MapContainer />
+              </Grid>
+            </section>
+            <section className="section parallax bg2" />
+            <section className="section">
+              <Grid item xs={12}>
+                <MissionStatement />
+              </Grid>
+            </section>
+            {/* <section className="section parallax bg4" /> */}
+          </div>
+        </Grid>
       </div>
     );
   }
