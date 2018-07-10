fd5080ee8defe509f967f9efa5018bec8e8b3277
DanFein <danfeinstat@gmail.com>
2018-07-09 21:34:46 -0700

    added a mission statement

---
+++ client/src/components/MissionStatement/MissionStatement.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const MissionStatement = props => (
+  <div className="mission-statement-wrapper">
+    {/* <div className="col-sm-3" /> */}
+    <div className="col-sm-6 mx-auto statement-box">
+      <h3 className="mx-auto mt-3 text-center bold underline">
+        Mission Statement
+      </h3>
+      <h5 className="mx-auto text-center bold">
+        We love food trucks, but finding them can be a chore. No longer!
+      </h5>
+      <h5 className="mx-auto text-center bold">
+        Hambre: bringing foodies to food trucks.
+      </h5>
+    </div>
+    {/* <div className="col-sm-3" /> */}
+  </div>
+);
